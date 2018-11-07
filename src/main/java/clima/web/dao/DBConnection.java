c3d97bf5ea77eb985240d42ef0d5934d3d9ce577
Java <Java@NC0952>
2018-11-07 20:42:23 -0300

    Integracion web con base de datos

---
+++ src/main/java/clima/web/dao/DBConnection.java
@@ -0,0 +1,70 @@
+package clima.web.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import clima.web.model.Usuario;
+
+public class DBConnection {
+
+	public void testDBConnection() {
+		System.out.println("-------- MySQL JDBC Connection Testing ------------");
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Where is your MySQL JDBC Driver?");
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		System.out.println("MySQL JDBC Driver Registered!");
+	}
+
+	/*
+	 * https://github.com/mlennard-utn/avanzando/blob/master/AvanzadoRepaso/src/jdbc
+	 * /ConexionDB.java
+	 */
+	public Usuario getUsuarioByEmail(String email) throws SQLException {
+
+		Usuario user = null;
+
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_web", "javaweb",
+				"javaweb");) {
+
+			String query = "SELECT * FROM usuario where email = ?";
+			PreparedStatement pstmt = connection.prepareStatement(query);
+
+			pstmt.setString(1, email);
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				user = new Usuario();
+				String password = rs.getString("password");
+				user.setPassword(password);
+				user.setEmail(email);
+
+			}
+
+		} catch (SQLException ex) {
+			while (ex != null) {
+				System.out.println("SQLState:  " + ex.getSQLState());
+				System.out.println("Error Code:" + ex.getErrorCode());
+				System.out.println("Message:   " + ex.getMessage());
+				Throwable t = ex.getCause();
+				while (t != null) {
+					System.out.println("Cause:" + t);
+					t = t.getCause();
+				}
+				ex = ex.getNextException();
+				throw ex;
+			}
+		}
+		return user;
+	}
+
+}
--- src/main/java/clima/web/model/Usuario.java
+++ src/main/java/clima/web/model/Usuario.java
@@ -3,7 +3,9 @@ package clima.web.model;
 public class Usuario {
 	
 	private String name;
-
+	private String email;
+	private String password;
+	
 	public String getName() {
 		return name;
 	}
@@ -11,6 +13,24 @@ public class Usuario {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 	
 	
 }
